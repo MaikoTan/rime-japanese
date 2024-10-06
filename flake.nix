ffd6e5968a1efa4a65f39c691e6925597f232ffa
Maiko Tan <maiko.tan.coding@gmail.com>
2024-10-06 17:24:19 +0800

    fix: flake structure

--- flake.nix
+++ flake.nix
@@ -1,27 +1,38 @@
 {
   description = "Japanese input method for rime";
 
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.rime-japanese = {
     url = "github:gkovacs/rime-japanese";
     flake = false;
   };
-  outputs = { self, nixpkgs, rime-japanese }:
-  {
-    packages.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux; stdenv.mkDerivation {
-      name = "rime-japanese";
-      version = "0.0.1";
+  outputs = { self, nixpkgs, flake-utils, rime-japanese }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.default = pkgs.stdenv.mkDerivation {
+        name = "rime-japanese";
 
-      nativeBuildInputs = [ pkgs.librime ];
+        src = pkgs.fetchFromGitHub {
+          owner = "gkovacs";
+          repo = "rime-japanese";
+          rev = "master";
+          sha256 = "sha256-/mIIyCu8V95ArKo/vIS3qAiD8InUmk8fAF/wejxRxGw=";
+        };
 
-      buildPhase = ''
-        for _s in $(ls *.schema.yaml); do rime_deployer --compile $_s; done
-      '';
+        nativeBuildInputs = with pkgs; [ librime rime-data ];
 
-      installPhase = ''
-        mkdir -p $out/share/rime-data/build
-        ln -sv ${rime-japanese}/*.yaml $out/share/rime-data/build/
-        ln -sv ${rime-japanese}/build/* $out/share/rime-data/build/build
-      '';
-    };
-  };
+        buildPhase = ''
+          for _s in $(ls *.schema.yaml); do rime_deployer --compile $_s . ${pkgs.rime-data}/share/rime-data/ .; done
+        '';
+
+        installPhase = ''
+          mkdir -p $out/share/rime-data/build
+          cp -v ${rime-japanese}/*.yaml $out/share/rime-data/
+          cp -v ${rime-japanese}/build/* $out/share/rime-data/build/
+        '';
+      };
+    }
+  );
 }
